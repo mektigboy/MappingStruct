aa6211d2c33699def297154f81a86e72336bb14d
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-10 16:30:05 -0500

    Update MappingStuct.sol

--- MappingStruct.sol
+++ MappingStruct.sol
@@ -2,14 +2,20 @@
 
 pragma solidity ^0.8.13;
 
-contract MappingStructExample {
+contract MappingStruct {
+    mapping(address => uint) public balanceReceived;
+
     function getBalance() public view returns(uint) {
         return address(this).balance;
     }
     function sendMoney() public payable {
-
+        balanceReceived[msg.sender] += msg.value;
     }
-    function withdrawAllMoney(address payable _to) public{
-        _to.transfer(address(this).balance);
+    function withdrawAllMoney(address payable _to) public {
+        uint balanceToSend = balanceReceived[msg.sender];
+
+        balanceReceived[msg.sender] = 0;
+
+        _to.transfer(balanceToSend);
     }
 }
