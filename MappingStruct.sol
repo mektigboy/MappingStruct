93264da09c0033ff2f48ac4eff089304cca1cff9
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-10 16:37:58 -0500

    Update MappingStuct.sol

--- MappingStruct.sol
+++ MappingStruct.sol
@@ -11,6 +11,13 @@ contract MappingStruct {
     function sendMoney() public payable {
         balanceReceived[msg.sender] += msg.value;
     }
+    function withdrawMoney(address payable _to, uint _amount) public {
+        require(balanceReceived[msg.sender] >= _amount, "Not enough funds."); // Check if balance has enough funds.
+
+        balanceReceived[msg.sender] -= _amount;
+
+        _to.transfer(_amount);
+    }
     function withdrawAllMoney(address payable _to) public {
         uint balanceToSend = balanceReceived[msg.sender];
 
